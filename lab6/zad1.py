d13601bb431ab94694f6baf8e2829b4f23755608
Maciej <gorski.maciej@o2.pl>
2019-01-02 17:31:23 +0100

    Changed the way the script works a little bit

--- lab6/zad1.py
+++ lab6/zad1.py
@@ -6,19 +6,21 @@ import numpy as np
 from skimage import io
 from skimage.color import rgb2gray as czary_mary_op_już_szary
 
-images = [(255 - io.imread('horse.png')).astype(np.bool), (255 - io.imread('psio.png')).astype(np.bool), (255 - io.imread('bw.png')).astype(np.bool)]
+_images = [(255 - io.imread('horse.png')), (255 - io.imread('psio.png')), (255 - io.imread('bw.png'))]
+images = [i.astype(np.bool) for i in _images]
 grey = [czary_mary_op_już_szary(i) for i in images]
 sp00ky_scary_skeletons = [morph.skeletonize(i) for i in grey]
-median = [morph.medial_axis(i) for i in grey]
+medial = [morph.medial_axis(i) for i in [czary_mary_op_już_szary(j) for j in _images]]
+
 for i in sp00ky_scary_skeletons:
     plt.imshow(i, cmap='gray')
     plt.show()
-for i in median:
+for i in medial:
     plt.imshow(i, cmap='gray')
     plt.show()
 
 for c, i in enumerate(sp00ky_scary_skeletons, start = 1):
     io.imsave('skeletonized' + str(c) + '.png', i.astype(np.uint8) * 255)
 
-for c, i in enumerate(median, start = 1):
+for c, i in enumerate(medial, start = 1):
     io.imsave('medial_axis' + str(c) + '.png', i.astype(np.uint8) * 255)
